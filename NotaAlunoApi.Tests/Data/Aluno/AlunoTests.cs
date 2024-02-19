2b9d3c4ce28d0ad624e90c71e6b56a87b118bcdb
VarleyS <varleysr@gmail.com>
2024-02-19 17:53:25 -0300

    Criado projeto de teste unitários

---
+++ NotaAlunoApi.Tests/Data/Aluno/AlunoTests.cs
@@ -0,0 +1,26 @@
+﻿using NotaAlunoApi.Data.Dto;
+using NotaAlunoApi.Model;
+
+namespace NotaAlunoApi.Tests.Data.Aluno
+{
+    public class AlunoTests
+    {
+        [Fact]
+        public void Create_Aluno_Falso() 
+        {
+            var aluno1 = new Model.Aluno()
+            {
+                Name = "A",
+                DataNascimento = "31/12/1991",
+                CPF = "100.223.076-48",
+                RG = "16.085.397",
+                Sexo = "Masculino",
+                Telefone = "(31)99878-7290"
+            };
+
+            Assert.Equal("A", aluno1.Name);
+            Assert.Equal("31/12/1991", aluno1.DataNascimento);
+            Assert.Equal("100.223.076-48", aluno1.CPF);
+        }
+    }
+}
