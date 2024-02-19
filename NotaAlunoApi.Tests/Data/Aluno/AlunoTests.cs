e2a29777475bca0d359a273df4ac2ea4fa902ca8
VarleyS <varleysr@gmail.com>
2024-02-19 18:37:43 -0300

    Implementando teste

--- NotaAlunoApi.Tests/Data/Aluno/AlunoTests.cs
+++ NotaAlunoApi.Tests/Data/Aluno/AlunoTests.cs
@@ -6,11 +6,11 @@ namespace NotaAlunoApi.Tests.Data.Aluno
     public class AlunoTests
     {
         [Fact]
-        public void Create_Aluno_Falso() 
+        public void Create_Aluno_Correto() 
         {
-            var aluno1 = new Model.Aluno()
+            var aluno = new Model.Aluno()
             {
-                Name = "A",
+                Name = "Varley",
                 DataNascimento = "31/12/1991",
                 CPF = "100.223.076-48",
                 RG = "16.085.397",
@@ -18,9 +18,33 @@ namespace NotaAlunoApi.Tests.Data.Aluno
                 Telefone = "(31)99878-7290"
             };
 
-            Assert.Equal("A", aluno1.Name);
-            Assert.Equal("31/12/1991", aluno1.DataNascimento);
-            Assert.Equal("100.223.076-48", aluno1.CPF);
+            Assert.Equal("Varley", aluno.Name);
+            Assert.Equal("31/12/1991", aluno.DataNascimento);
+            Assert.Equal("100.223.076-48", aluno.CPF);
+            Assert.Equal("16.085.397", aluno.RG);
+            Assert.Equal("Masculino", aluno.Sexo);
+            Assert.Equal("(31)99878-7290", aluno.Telefone);
+        }
+
+        [Fact]
+        public void Create_Aluno_Incorreto()
+        {
+            var aluno = new Model.Aluno()
+            {
+                Name = "",
+                DataNascimento = "",
+                CPF = "",
+                RG = "",
+                Sexo = "",
+                Telefone = ""
+            };
+
+            Assert.Equal("", aluno.Name);
+            Assert.Equal("", aluno.DataNascimento);
+            Assert.Equal("", aluno.CPF);
+            Assert.Equal("", aluno.RG);
+            Assert.Equal("", aluno.Sexo);
+            Assert.Equal("", aluno.Telefone);
         }
     }
 }
