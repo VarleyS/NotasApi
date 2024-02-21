185d744c03753acb52896beda54de14f0ce46c43
VarleyS <varleysr@gmail.com>
2024-02-20 21:32:42 -0300

    Implementando testes

--- NotaAlunoApi.Tests/Data/Aluno/AlunoTests.cs
+++ NotaAlunoApi.Tests/Data/Aluno/AlunoTests.cs
@@ -6,24 +6,24 @@ namespace NotaAlunoApi.Tests.Data.Aluno
     public class AlunoTests
     {
         [Fact]
-        public void Create_Aluno_Correto() 
+        public void Create_Aluno_Correto()
         {
             var aluno = new Model.Aluno()
             {
                 Name = "Varley",
-                DataNascimento = "31/12/1991",
-                CPF = "100.223.076-48",
-                RG = "16.085.397",
+                DataNascimento = "11/22/3333",
+                CPF = "111.222.333-44",
+                RG = "11.222.333",
                 Sexo = "Masculino",
-                Telefone = "(31)99878-7290"
+                Telefone = "(00)12345-6789"
             };
 
             Assert.Equal("Varley", aluno.Name);
-            Assert.Equal("31/12/1991", aluno.DataNascimento);
-            Assert.Equal("100.223.076-48", aluno.CPF);
-            Assert.Equal("16.085.397", aluno.RG);
+            Assert.Equal("11/22/3333", aluno.DataNascimento);
+            Assert.Equal("111.222.333-44", aluno.CPF);
+            Assert.Equal("11.222.333", aluno.RG);
             Assert.Equal("Masculino", aluno.Sexo);
-            Assert.Equal("(31)99878-7290", aluno.Telefone);
+            Assert.Equal("(00)12345-6789", aluno.Telefone);
         }
 
         [Fact]
@@ -45,6 +45,160 @@ namespace NotaAlunoApi.Tests.Data.Aluno
             Assert.Equal("", aluno.RG);
             Assert.Equal("", aluno.Sexo);
             Assert.Equal("", aluno.Telefone);
+        }
+
+        [Fact]
+        public void Nome_nao_nulo()
+        {
+            var aluno = new Model.Aluno()
+            {
+                Name = "",
+                DataNascimento = "11/22/3333",
+                CPF = "111.222.333-44",
+                RG = "11.222.333",
+                Sexo = "Masculino",
+                Telefone = "(00)12345-6789"
+            };
+
+            if (aluno.Name == null)
+            {
+                Assert.Equal("", aluno.Name);
+            }
+            Assert.Equal("11/22/3333", aluno.DataNascimento);
+            Assert.Equal("111.222.333-44", aluno.CPF);
+            Assert.Equal("11.222.333", aluno.RG);
+            Assert.Equal("Masculino", aluno.Sexo);
+            Assert.Equal("(00)12345-6789", aluno.Telefone);
+        }
+
+        [Fact]
+        public void DataNascimento_nao_nulo()
+        {
+            var aluno = new Model.Aluno()
+            {
+                Name = "Varley",
+                DataNascimento = "",
+                CPF = "111.222.333-44",
+                RG = "11.222.333",
+                Sexo = "Masculino",
+                Telefone = "(00)12345-6789"
+            };
+
+            Assert.Equal("Varley", aluno.Name);
+
+            if (aluno.DataNascimento == null)
+            {
+                Assert.Equal("", aluno.DataNascimento);
+            }
+
+            Assert.Equal("111.222.333-44", aluno.CPF);
+            Assert.Equal("11.222.333", aluno.RG);
+            Assert.Equal("Masculino", aluno.Sexo);
+            Assert.Equal("(00)12345-6789", aluno.Telefone);
+        }
+
+        [Fact]
+        public void Cpf_nao_nulo()
+        {
+            var aluno = new Model.Aluno()
+            {
+                Name = "Varley",
+                DataNascimento = "11/22/3333",
+                CPF = "",
+                RG = "11.222.333",
+                Sexo = "Masculino",
+                Telefone = "(00)12345-6789"
+            };
+
+            Assert.Equal("Varley", aluno.Name);
+            Assert.Equal("11/22/3333", aluno.DataNascimento);
+
+            if (aluno.CPF == null)
+            {
+                Assert.Equal("", aluno.CPF);
+            }
+
+            Assert.Equal("11.222.333", aluno.RG);
+            Assert.Equal("Masculino", aluno.Sexo);
+            Assert.Equal("(00)12345-6789", aluno.Telefone);
+        }
+
+        [Fact]
+        public void Rg_nao_nulo()
+        {
+            var aluno = new Model.Aluno()
+            {
+                Name = "Varley",
+                DataNascimento = "11/22/3333",
+                CPF = "111.222.333-44",
+                RG = "",
+                Sexo = "Masculino",
+                Telefone = "(00)12345-6789"
+            };
+
+            Assert.Equal("Varley", aluno.Name);
+            Assert.Equal("11/22/3333", aluno.DataNascimento);
+            Assert.Equal("111.222.333-44", aluno.CPF);
+
+            if (aluno.RG == null)
+            {
+                Assert.Equal("", aluno.RG);
+            }
+
+            Assert.Equal("Masculino", aluno.Sexo);
+            Assert.Equal("(00)12345-6789", aluno.Telefone);
+        }
+
+        [Fact]
+        public void Sexo_nao_nulo()
+        {
+            var aluno = new Model.Aluno()
+            {
+                Name = "Varley",
+                DataNascimento = "11/22/3333",
+                CPF = "111.222.333-44",
+                RG = "11.222.333",
+                Sexo = "",
+                Telefone = "(00)12345-6789"
+            };
+
+            Assert.Equal("Varley", aluno.Name);
+            Assert.Equal("11/22/3333", aluno.DataNascimento);
+            Assert.Equal("111.222.333-44", aluno.CPF);
+            Assert.Equal("11.222.333", aluno.RG);
+
+            if (aluno.Sexo == null)
+            {
+                Assert.Equal("", aluno.Sexo);
+            }
+
+            Assert.Equal("(00)12345-6789", aluno.Telefone);
+        }
+
+        [Fact]
+        public void Telefone_nao_nulo()
+        {
+            var aluno = new Model.Aluno()
+            {
+                Name = "Varley",
+                DataNascimento = "11/22/3333",
+                CPF = "111.222.333-44",
+                RG = "11.222.333",
+                Sexo = "Masculino",
+                Telefone = ""
+            };
+
+            Assert.Equal("Varley", aluno.Name);
+            Assert.Equal("11/22/3333", aluno.DataNascimento);
+            Assert.Equal("111.222.333-44", aluno.CPF);
+            Assert.Equal("11.222.333", aluno.RG);
+            Assert.Equal("Masculino", aluno.Sexo);
+
+            if (aluno.Telefone == null)
+            {
+                Assert.Equal("", aluno.Telefone);
+            }
+
         }
     }
 }
